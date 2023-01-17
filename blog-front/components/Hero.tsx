b6606148b5f3fb525386b8670524a32126eacf36
Loner1024 <xth12138@gmail.com>
2023-01-17 13:36:16 +0800

    refactor(blog):hero alt

--- blog-front/components/Hero.tsx
+++ blog-front/components/Hero.tsx
@@ -5,7 +5,7 @@ import DemoImg from "../public/001.png"
 const Hero = () => {
     return (
         <div className="max-w-[1240px] mx-auto relative flex justify-center z-[2] h-48 md:h-96 my-10">
-            <Image width="100%" height="100%" style={{objectFit: 'cover'}} src={DemoImg.src}/>
+            <Image width="100%" height="100%" alt="banner" style={{objectFit: 'cover'}} src={DemoImg.src}/>
         </div>
     );
 };
